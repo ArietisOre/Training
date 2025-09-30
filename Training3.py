933ca1e02ae70109f0f94712f237bbd1c0787200
ArietisOre <sta321@seznam.cz>
2025-09-30 15:28:19 +0200

    „Přidal jsem print na řádku 6."

---
+++ Training3.py
@@ -0,0 +1,6 @@
+jmeno = "Marek"
+heslo = "1234"
+uzivatel = {"Marek": "1234"}
+if jmeno in uzivatel and uzivatel[jmeno]== heslo:
+    print("Přihlášení problehlo úspěšně.")
+    print("Vítejte: ", jmeno)
